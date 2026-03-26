a062c58fb15f7664c4631346ce61dd5423ce9213
Ashutosh Dash <dashashutosh2007@gmail.com>
2026-03-26 08:26:03 +0530

    Update author name in UC4_HelloApp.java

--- UC4_HelloApp.java
+++ UC4_HelloApp.java
@@ -9,7 +9,7 @@
  * - If names are provided, it will display "Hello, [Name1], [Name2], ...!"
  * - If no names are provided, it will display "Hello, World!"
  *
- * @author Developer Name
+ * @author Ashutosh
  * @version 4.0
  * @since UC1
  */
